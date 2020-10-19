17956425a175fc2de5c9c12cddbdcfb7a54be058
Tyler Woodfin <tylerjwoodfin@gmail.com>
2020-10-18 21:00:45 -0500

    Updated tasks.php to be more user-friendly

--- tasks.php
+++ tasks.php
@@ -1,8 +1,11 @@
 <?php
-    $tasksFile = fopen("secure/TasksFile", "r") or die("Unable to open file!");
-    echo fread($tasksFile,filesize("secure/TasksFile"));
 
-    // Replace this line to match the syntax:
-    // rclone copy {your Rclone remote name}:{your file path} /var/www/html/secure
-    echo shell_exec('rclone copy ' + fread($tasksFile,filesize("secure/TasksFile") + ' /var/www/html/secure/cache');
+    // If you're getting errors for this file, go to secure, create a file called "TasksFile", and
+    // check that it points to the correct file on Dropbox. For example, mine is "Notes/Tasks.txt" without the quotes.
+    // Don't edit this file.
+    
+    $tasksFile = fopen("secure/TasksFile", "r") or die("Unable to open file!");
+    $tasksFile = fread($tasksFile,filesize("secure/TasksFile"));
+
+    echo shell_exec("rclone copy Dropbox:$tasksFile /var/www/html/secure/cache");
 ?> 
