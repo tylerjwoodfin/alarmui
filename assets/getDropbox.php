227e577427068c9ec8174b5a77999e1e375a3b65
Tyler Woodfin <tylerjwoodfin@gmail.com>
2020-10-24 15:45:37 -0500

    Fixed getDropbox.php

--- assets/getDropbox.php
+++ assets/getDropbox.php
@@ -1,6 +1,6 @@
 <?php
 
-    $filepath = "secure/" + $_GET['path'];  // this is coming from update.js and should either be "TasksFile" or "BedtimeFile" 
+    $filepath = "/var/www/html/secure/" . $_GET['path'];  // this is coming from update.js and should either be "TasksFile" or "BedtimeFile" 
 
     // If you're getting errors for this file, go to secure, create a file called "TasksFile", and
     // check that it points to the correct file on Dropbox. For example, mine is "Notes/Tasks.txt" without the quotes.
@@ -8,6 +8,8 @@
     
     $tasksFile = fopen($filepath, "r") or die("Unable to open file!");
     $tasksFile = fread($tasksFile,filesize($filepath));
+
+    echo $tasksFile;
 
     echo shell_exec("rclone copy Dropbox:$tasksFile /var/www/html/secure/cache");
 ?> 
