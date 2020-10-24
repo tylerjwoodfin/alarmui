55febeec266a926c94ad5f1d94af80c8dff8098b
Tyler Woodfin <tylerjwoodfin@gmail.com>
2020-10-24 16:08:52 -0500

    Changed getTasks in update.js to generic "getDropboxFile"

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ make WeatherAPI
 echo "# Enter your OpenWeather API on the line below ↓ You can get this for free at https://openweathermap.org/api." >> WeatherAPI
 
 make TasksFile
-echo "# Enter the Dropbox path to your To Do list in the line below ↓ (example: Notes/ToDo.txt)" >> TasksFile
+make BedtimeFile
 
 make Greeting
 echo "# This file can have 2 non-comment/blank lines: the top for a headline and the bottom for a subheadline.\nUse Greeting Mode to welcome guests.\n# Blank lines, like the one above, aren't counted.\nChange your message in /secure/Greeting." >> Greeting
